fa838ad6e6dbdb3a9f1c4392d498704985d05119
Ricardo <rntutumu@bornan.net>
2022-10-24 13:42:02 +0200

    ADD crcf Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Main from './components/Main/Main';
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import React,{ useState, useEffect } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import './styles/styles.scss'
+import axios from 'axios';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Wheather App</h1>
-      </header>
+      <BrowserRouter>
+        <Header/>
+        <Main/>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Main/Main.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+  render() {
+    return <div className="text-primary">Esto es el Main</div>;
+  }
+}
+
+export default Main;
